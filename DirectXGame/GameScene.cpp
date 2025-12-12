4e5924181cd7303e52159c2551da21b013d3503a
Kamata Satoshi <k025g0080@g.neec.ac.jp>
2025-12-12 16:02:33 +0900

    KamataEngineを入れた

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,18 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+/// <summary>
+/// 初期化
+/// </summary>
+void GameScene::Initialize() {}
+
+/// <summary>
+/// 更新
+/// </summary>
+void GameScene::Update() {}
+
+/// <summary>
+/// 描画
+/// </summary>
+void GameScene::Draw() {}
